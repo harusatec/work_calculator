29239917fa67a149769094764e427411ceabd16e
haru S <hstec2121@gmail.com>
2019-11-08 14:14:21 +0900

    satoharu add sound

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.mycalculator
 
+import android.media.AudioAttributes
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +15,112 @@ class MainActivity : AppCompatActivity() {
     val numberList = ArrayList<Double>()
     val operationList = ArrayList<Char>()
 
+    // 効果音用
+    private lateinit var soundPool: SoundPool
+    private var soundA = 0
+    private var soundB = 0
+    private var soundC = 0
+    private var soundD = 0
+    private var soundE = 0
+    private var soundF = 0
+    private var soundG = 0
+    private var soundH = 0
+    private var soundI = 0
+    private var soundJ = 0
+    private var soundK = 0
+    private var soundL = 0
+    private var soundM = 0
+    private var soundN = 0
+    private var soundO = 0
+    private var soundP = 0
+    private var soundQ = 0
+    private var soundR = 0
+    private var soundS = 0
+    private var soundT = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val audioAttributes = AudioAttributes.Builder()
+            // USAGE_MEDIA or USAGE_GAME
+            .setUsage(AudioAttributes.USAGE_GAME)
+            // CONTENT_TYPE_MUSIC or CONTENT_TYPE_SPEECH, etc.
+            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            .build()
+
+        soundPool = SoundPool.Builder().setAudioAttributes(audioAttributes)
+            // ストリーム数に応じて
+            .setMaxStreams(20)
+            .build()
+
+        // boyoyon1.mp3 をロードしておく
+        soundA = soundPool.load(this, R.raw.boyoyon1, 1)
+
+        // woodfish1.mp3 をロードしておく
+        soundB = soundPool.load(this, R.raw.woodfish1, 1)
+
+        // broadcastingstart1.mp3 をロードしておく
+        soundC = soundPool.load(this, R.raw.broadcastingstart1, 1)
+
+        // peopleperformanceuwaa1.mp3 をロードしておく
+        soundD = soundPool.load(this, R.raw.peopleperformanceuwaa1, 1)
+
+        // peoplestudiolaughlarge2.mp3 をロードしておく
+        soundE = soundPool.load(this, R.raw.peoplestudiolaughlarge2, 1)
+
+        // machinegun1.mp3 をロードしておく
+        soundF = soundPool.load(this, R.raw.machinegun1, 1)
+
+        // conch1.mp3 をロードしておく
+        soundG = soundPool.load(this, R.raw.conch1, 1)
+
+        // question1.mp3 をロードしておく
+        soundH = soundPool.load(this, R.raw.question1, 1)
+
+        // drumroll1.mp3 をロードしておく
+        soundI = soundPool.load(this, R.raw.drumroll1, 1)
+
+        // walkgeta1.mp3 をロードしておく
+        soundJ = soundPool.load(this, R.raw.walkgeta1, 1)
+
+        // walktussock1.mp3 をロードしておく
+        soundK = soundPool.load(this, R.raw.walktussock1, 1)
+
+        // dooropen1.mp3 をロードしておく
+        soundL = soundPool.load(this, R.raw.dooropen1, 1)
+
+        // money1.mp3 をロードしておく
+        soundM = soundPool.load(this, R.raw.money1, 1)
+
+        // aburazemicry1.mp3 をロードしておく
+        soundN = soundPool.load(this, R.raw.aburazemicry1, 1)
+
+        // thunder1.mp3 をロードしておく
+        soundO = soundPool.load(this, R.raw.thunder1, 1)
+
+        // catcry1.mp3 をロードしておく
+        soundP = soundPool.load(this, R.raw.catcry1, 1)
+
+        // elephant1.mp3 をロードしておく
+        soundQ = soundPool.load(this, R.raw.elephant1, 1)
+
+        // wave2.mp3 をロードしておく
+        soundR = soundPool.load(this, R.raw.wave2, 1)
+
+        // gameswordmanstart1.mp3 をロードしておく
+        soundS = soundPool.load(this, R.raw.gameswordmanstart1, 1)
+
+        // question2.mp3 をロードしておく
+        soundT = soundPool.load(this, R.raw.question2, 1)
+
+        // load が終わったか確認する場合
+        soundPool.setOnLoadCompleteListener{ soundPool, sampleId, status ->
+            Log.d("debug", "sampleId=$sampleId")
+            Log.d("debug", "status=$status")
+        }
+
 
         /*******
          * 0 ～ 9 ボタンクリック時
@@ -21,50 +128,82 @@ class MainActivity : AppCompatActivity() {
         buttonOf0.setOnClickListener {
             formula.text = "${formula.text}0"  // 表示値に 0 をAdd
             calcStr += "0"                     // 数字文字列に 0 をAdd
+
+            // soundA(boyoyon1.mp3 ) の再生
+            soundPool.play(soundA, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         buttonOf1.setOnClickListener {
             formula.text = "${formula.text}1"  // 表示値に 1 をAdd
             calcStr += "1"                   // 数字文字列に 1 をAdd
+
+            // soundB(woodfish1.mp3) の再生
+            soundPool.play(soundB, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         buttonOf2.setOnClickListener {
             formula.text = "${formula.text}2"  // 表示値に 2 をAdd
             calcStr += "2"                   // 数字文字列に 2 をAdd
+
+            // soundC(broadcastingstart1.mp3) の再生
+            soundPool.play(soundC, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         buttonOf3.setOnClickListener {
             formula.text = "${formula.text}3"  // 表示値に 3 をAdd
             calcStr += "3"                   // 数字文字列に 3 をAdd
+
+            // soundD(peopleperformanceuwaa1.mp3) の再生
+            soundPool.play(soundD, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         buttonOf4.setOnClickListener {
             formula.text = "${formula.text}4"  // 表示値に 4 をAdd
             calcStr += "4"                   // 数字文字列に 4 をAdd
+
+            // soundE(peoplestudiolaughlarge2.mp3) の再生
+            soundPool.play(soundE, 1.0f, 1.0f, 1, 0, 1.0f)
         }
+
+
         buttonOf5.setOnClickListener {
             formula.text = "${formula.text}5"  // 表示値に 5 をAdd
             calcStr += "5"                   // 数字文字列に 5 をAdd
+
+            // soundF(machinegun1.mp3) の再生
+            soundPool.play(soundF, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         buttonOf6.setOnClickListener {
             formula.text = "${formula.text}6"  // 表示値に 6 をAdd
             calcStr += "6"                   // 数字文字列に 6 をAdd
+
+            // soundG(conch1.mp3) の再生
+            soundPool.play(soundG, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         buttonOf7.setOnClickListener {
             formula.text = "${formula.text}7"  // 表示値に 7 をAdd
             calcStr += "7"                   // 数字文字列に 7 をAdd
+
+            // soundH(question1.mp3) の再生
+            soundPool.play(soundH, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         buttonOf8.setOnClickListener {
             formula.text = "${formula.text}8"  // 表示値に 8 をAdd
             calcStr += "8"                   // 数字文字列に 8 をAdd
+
+            // soundI(drumroll1.mp3) の再生
+            soundPool.play(soundI, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         buttonOf9.setOnClickListener {
             formula.text = "${formula.text}9"  // 表示値に 9 をAdd
             calcStr += "9"                   // 数字文字列に 9 をAdd
+
+            // soundJ(walkgeta1.mp3) の再生
+            soundPool.play(soundJ, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
 
@@ -74,6 +213,9 @@ class MainActivity : AppCompatActivity() {
         buttonOfComma.setOnClickListener {
             formula.text = "${formula.text}."  // 表示値に . をAdd
             calcStr += "."                   // 数字文字列に . をAdd
+
+            // soundK(walktussock1.mp3) の再生
+            soundPool.play(soundK, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         /********
@@ -83,6 +225,9 @@ class MainActivity : AppCompatActivity() {
             formula.text = "${formula.text}+"
             addList(calcStr, '+')
             calcStr = ""
+
+            // soundL(dooropen1.mp3) の再生
+            soundPool.play(soundL, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         /********
@@ -92,6 +237,9 @@ class MainActivity : AppCompatActivity() {
             formula.text = "${formula.text}-"
             addList(calcStr, '-')
             calcStr = ""
+
+            // soundM(money1.mp3) の再生
+            soundPool.play(soundM, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         /********
@@ -101,6 +249,9 @@ class MainActivity : AppCompatActivity() {
             formula.text = "${formula.text}*"
             addList(calcStr, '*')
             calcStr = ""
+
+            // soundN(aburazemicry1.mp3) の再生
+            soundPool.play(soundN, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         /********
@@ -110,6 +261,9 @@ class MainActivity : AppCompatActivity() {
             formula.text = "${formula.text}/"
             addList(calcStr, '/')
             calcStr = ""
+
+            // soundO(thunder1.mp3) の再生
+            soundPool.play(soundO, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         /*******
@@ -126,6 +280,9 @@ class MainActivity : AppCompatActivity() {
                 calcStr = calcStr.substring(0, calcStr.lastIndex)
             }
 
+            // soundP(catcry1.mp3) の再生
+            soundPool.play(soundP, 1.0f, 1.0f, 1, 0, 1.0f)
+
         }
 
         /*******
@@ -133,6 +290,9 @@ class MainActivity : AppCompatActivity() {
          *******/
         percentButton.setOnClickListener {
             formula.text = "${formula.text}%"
+
+            // soundQ(elephant1.mp3) の再生
+            soundPool.play(soundQ, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         /*******
@@ -140,6 +300,8 @@ class MainActivity : AppCompatActivity() {
          *******/
         plusMinusButton.setOnClickListener {
 
+            // soundR(wave2.mp3) の再生
+            soundPool.play(soundR, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         /*******
@@ -150,6 +312,9 @@ class MainActivity : AppCompatActivity() {
             calcStr = ""
             numberList.clear()
             operationList.clear()
+
+            // soundS(gameswordmanstart1.mp3) の再生
+            soundPool.play(soundS, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
         /********
@@ -164,6 +329,9 @@ class MainActivity : AppCompatActivity() {
             calcStr = result
             numberList.clear()
             operationList.clear()
+
+            // soundT(question2.mp3) の再生
+            soundPool.play(soundT, 1.0f, 1.0f, 1, 0, 1.0f)
         }
 
     }
